10b2df6f341e849d2f59ea7a4b9d69af2f12ad9f
www.lapshop.in <1>
2019-03-09 04:39:39 +0530

    initial setup

---
+++ Downloads/MindOrksBootCampInstagramProject/app/src/main/java/com/example/wwwlapshopin/mindorks_bootcamp_instagram_project/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wwwlapshopin.mindorks_bootcamp_instagram_project
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
